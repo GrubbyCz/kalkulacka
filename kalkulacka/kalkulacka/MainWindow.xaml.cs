cb78a441942136b6a81f2f0a2628747f0311687f
Adam Němeček <nemeceka22@spstrutnov.cz>
2024-01-15 12:25:45 +0100

    nove tlacitka

--- kalkulacka/kalkulacka/MainWindow.xaml.cs
+++ kalkulacka/kalkulacka/MainWindow.xaml.cs
@@ -24,5 +24,71 @@ namespace kalkulacka
         {
             InitializeComponent();
         }
+        
+        private void Button_Click_0(object sender, RoutedEventArgs e)
+        {
+            textik.Text = textik.Text + "0";
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            textik.Text = textik.Text + "1";
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            textik.Text = textik.Text + "2";
+        }
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            textik.Text = textik.Text + "3";
+        }
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            textik.Text = textik.Text + "4";
+        }
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            textik.Text = textik.Text + "5";
+        }
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            textik.Text = textik.Text + "6";
+        }
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            textik.Text = textik.Text + "7";
+        }
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            textik.Text = textik.Text + "8";
+        }
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            textik.Text = textik.Text + "9";
+        }
+        
+        
+        private void Button_Click_C(object sender, RoutedEventArgs e)
+        {
+            textik.Text = "";
+        }
+        private void Button_Click_zpet(object sender, RoutedEventArgs e)
+        {
+            textik.Text = "";
+        }
+        private void Button_Click_cara(object sender, RoutedEventArgs e)
+        {
+            textik.Text = textik.Text + ",";
+        }
+
+
+
+
+
+
+
+
+
+
     }
+
 }
